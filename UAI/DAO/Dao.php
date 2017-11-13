b04d297624815906106691f382f86400b4f826ef
Marildo Cesar <marildocesar@hotmail.com>
2017-11-13 20:25:23 -0200

    Add metdodo e teste de update

--- UAI/DAO/Dao.php
+++ UAI/DAO/Dao.php
@@ -7,36 +7,32 @@ use UAI\DAO\Connection;
 use UAI\DAO\Libs\Entity;
 use UAI\DAO\Libs\Atributes;
 
-class Dao
-{
+class Dao {
 
     private $conn;
     private $instanceConn;
     protected $entity;
     private $atributes;
-
     private $tableName;
     private $tableKey;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->conn = new Connection();
         $this->entity = new Entity();
         $this->atributes = new Atributes();
-        
-        $this->entity->tableName ='livros' ;
+
+        $this->entity->tableName = 'livros';
     }
 
-    public function create($pojo)
-    {        
+    public function create($pojo) {
         $this->init();
         $fields = $this->atributes->createFields($pojo);
         $values = $this->atributes->createValues($pojo);
         $bindParameter = $this->atributes->bindCreateParamenters($pojo);
-        
+
         $query = "INSERT INTO $this->tableName ($fields) VALUES ($values)";
         $pdo = $this->instanceConn->prepare($query);
-        
+
         try {
             $pdo->execute($bindParameter);
             return $this->instanceConn->lastInsertId();
@@ -45,12 +41,11 @@ class Dao
         }
     }
 
-    public function readByKey($key)
-    {
+    public function readByKey($key) {
         $this->init();
         $query = "SELECT * FROM $this->tableName WHERE $this->dbKey = '$key'";
         $pdo = $this->instanceConn->prepare($query);
-        
+
         try {
             $pdo->execute();
             return $pdo->fetch();
@@ -62,9 +57,9 @@ class Dao
     public function readByAtributes($atributes) {
         $this->init();
 
-        $where = $this->atributes->createWhere($atributes);      
+        $where = $this->atributes->createWhere($atributes);
 
-        $query = "SELECT * FROM $this->tableName $where";        
+        $query = "SELECT * FROM $this->tableName $where";
         $pdo = $this->instanceConn->prepare($query);
 
         try {
@@ -75,11 +70,37 @@ class Dao
         }
     }
 
+    public function update($pojo) {
 
-    private function init()
-    {
+        $this->init();
+        $keyValue = NULL;
+        $fields = $this->atributes->updateFields($pojo);
+
+        foreach ($pojo as $key => $value) {
+            if (strtolower($key) == strtolower($this->dbKey)) {
+                $keyValue = $value;
+                break;
+            }
+        }
+
+        $query = "UPDATE $this->tableName  SET $fields WHERE $this->dbKey=:key";
+        $pdo = $this->instanceConn->prepare($query);
+
+        $bindParameter = $this->atributes->bindCreateParamenters($pojo);
+        $bindParameter[':key'] = $keyValue;
+
+        try {
+            $pdo->execute($bindParameter);
+            return $pdo->rowCount();
+        } catch (PDOException $e) {
+            dump($e->getMessage());
+        }
+    }
+
+    private function init() {
         $this->instanceConn = $this->conn->connection();
         $this->tableName = $this->entity->tableName;
         $this->dbKey = $this->entity->key;
     }
+
 }
--- index.php
+++ index.php
@@ -13,15 +13,19 @@ use UAI\DAO\Dao;
 
 $dao = new Dao();
 
-/*
+/*  create e readByKey
 $novo = ['nome'=>'As cronicas de fogo','valor'=>'60'];
 $idNovo=$dao->create($novo);
 
 $dados = $dao->readByKey($idNovo);
 var_dump($dados);
 
-*/
-
+readByAtributes
 $att = ['valor'=>'60'];
 $dados = $dao->readByAtributes($att);
 var_dump($dados);
+*/
+
+$up = ['id'=>'3','valor'=>'40','nome'=>'Hobbit'];
+$rowsAf = $dao->update($up);
+var_dump($rowsAf);
